62388f633990afd2c207b07478c875473ec20da4
Knirag <105249761+Knirag@users.noreply.github.com>
2024-05-21 13:28:26 +0200

    ...

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -7,4 +7,25 @@ module.exports = {
     filename: "bundle.js",
   },
   mode: "production",
+  module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                  'file-loader',
+                  {
+                    loader: 'image-webpack-loader',
+                    options: {
+                      bypassOnDebug: true, // webpack@1.x
+                      disable: true, // webpack@2.x and newer
+                    },
+                  },
+                ],
+            }
+        ],
+    },
 };
