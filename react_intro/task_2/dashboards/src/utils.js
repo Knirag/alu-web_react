545240a8c6a0ac81c0c2bfb2daaeea9c5798fc5e
Knirag <105249761+Knirag@users.noreply.github.com>
2024-05-22 16:08:57 +0200

    ....

--- react_intro/task_2/dashboards/src/utils.js
+++ react_intro/task_2/dashboards/src/utils.js
@@ -1,7 +1,15 @@
-export const getFullYear = () => {
-    return new Date().getFullYear();
-};
+export function getFullYear() {
+  return new Date().getFullYear();
+}
 
-export const getFooterCopy = (isIndex) => {
-    return isIndex ? 'Holberton School' : 'Holberton School main dashboard';
-};
+export function getFooterCopy(isIndex) {
+  if (isIndex) {
+    return "Holberton School.";
+  } else {
+    return "Holberton School main dashboard";
+  }
+}
+
+export function getLatestNotification() {
+  return { __html: "<strong>Urgent requirement</strong> - complete by EOD" };
+}
