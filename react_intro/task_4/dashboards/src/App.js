63cefe1420251be49347e841793c058718196ea8
Knirag <105249761+Knirag@users.noreply.github.com>
2024-05-22 17:16:57 +0200

    ...

--- react_intro/task_4/dashboards/src/App.js
+++ react_intro/task_4/dashboards/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import logo from "./holberton-logo.png";
+import { getFullYear, getFooterCopy } from "./utils";
+import "./App.css";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="App-Header">
+        <img src={logo} className="holbertonLogo" />
+        <h1 className="holbertonHeaderName">School Dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <div className="form">
+          <label htmlFor="email">
+            <span>Email:</span>
+            <input type="email" name="email" id="email" />
+          </label>
+
+          <label htmlFor="password">
+            <span>Password:</span>
+            <input type="password" name="password" id="pwd" />
+          </label>
+
+          <button onClick={() => {}}>OK</button>
+        </div>
+      </div>
+      <div className="App-footer"></div>
+      <p>
+        Copyright {getFullYear()} {getFooterCopy(true)}
+      </p>
     </div>
   );
-}
+};
 
 export default App;
