43eaa1a899ea0e8eb7ad24f89004bdc3811199dd
Knirag <105249761+Knirag@users.noreply.github.com>
2024-06-15 20:12:04 +0200

    ....

--- 0x08_react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.js
+++ 0x08_react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.js
@@ -3,6 +3,8 @@ import {
   LOGOUT,
   DISPLAY_NOTIFICATION_DRAWER,
   HIDE_NOTIFICATION_DRAWER,
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE,
 } from "./uiActionTypes";
 
 export const login = (email, password) => {
@@ -32,3 +34,21 @@ export const hideNotificationDrawer = () => {
 };
 export const boundhideNotificationDrawer = (index) =>
   dispatch(hideNotificationDrawer(index));
+
+export const loginSuccess = () => {
+  return { type: LOGIN_SUCCESS };
+};
+
+export const loginFailure = () => {
+  return { type: LOGIN_FAILURE };
+};
+
+export const loginRequest = (email, password) => {
+  return function (dispatch) {
+    dispatch(login(email, password));
+    return fetch("http://localhost:3000/login-success.json")
+      .then((response) => response.json())
+      .then((json) => dispatch(loginSuccess()))
+      .catch((err) => dispatch(loginFailure()));
+  };
+};
